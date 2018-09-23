a016b23fcaf2ab84566d55f871e238f40dce0b6a
Nicolae BERENDEA <nberendea@ias.bitdefender.biz>
2018-09-23 14:06:05 +0300

    get ip

---
+++ first-network/utils/getContainerIp.py
@@ -0,0 +1,26 @@
+import sys
+import os
+import subprocess
+
+
+if len(sys.argv)!= 2:
+	print("Usage: {} <peer_name>".format(__file__))
+	raise SystemExit
+
+srvcName = sys.argv[1]
+
+def getContainerIP(service):
+	# node ID
+	cmd = "docker service ps --format '{{{{.Node}}}}' --no-resolve {srvc}".format(srvc=service)
+	p = subprocess.Popen([cmd], shell=True, stdout=subprocess.PIPE)
+	srvcId, _ = p.communicate()
+
+	# node IP
+	cmd = "docker node inspect --format '{{{{.Status.Addr}}}}' {srvc}".format(srvc=srvcId.strip())
+	p = subprocess.Popen([cmd], shell=True, stdout=subprocess.PIPE)
+	srvcIp, _ = p.communicate()
+
+	return srvcIp.strip()
+
+
+getContainerIP(srvcName)
