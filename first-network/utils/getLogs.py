06592d2716291adb175675ceaf05f66938d26465
Berendea Nicolae <berendeanicolae@gmail.com>
2018-10-02 10:52:08 +0000

    path fix

--- first-network/utils/getLogs.py
+++ first-network/utils/getLogs.py
@@ -1,3 +1,4 @@
+import os
 import time
 import subprocess
 
@@ -26,15 +27,19 @@ for service in services:
 	cmd = "ssh ubuntu@{ip} \"docker inspect {cnt} --format {{{{.LogPath}}}}\"".format(ip=ip, cnt=id)
 	p = subprocess.Popen([cmd], shell=True, stdout=subprocess.PIPE)
 	logPath, _ = p.communicate()
+	logPath = logPath.strip()
 
 	cmd = "ssh {ip}".format(ip=ip)
 	p = subprocess.Popen([cmd], stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True, universal_newlines=True, bufsize=0)
-	p.stdin.write("sudo chmod 777 {}\n".format(os.path.basename(os.path.basename(os.path.basename(os.path.basename(logPath))))))
-	p.stdin.write("sudo chmod 777 {}\n".format(os.path.basename(os.path.basename(os.path.basename(logPath)))))
-	p.stdin.write("sudo chmod 777 {}\n".format(os.path.basename(os.path.basename(logPath))))
-	p.stdin.write("sudo chmod 777 {}\n".format(os.path.basename(logPath)))
+	p.stdin.write("sudo chmod 777 {}\n".format(os.path.dirname(os.path.dirname(os.path.dirname(logPath)))))
+	p.stdin.write("sudo chmod 777 {}\n".format(os.path.dirname(os.path.dirname(logPath))))
+	p.stdin.write("sudo chmod 777 {}\n".format(os.path.dirname(logPath)))
+	p.stdin.write("sudo chmod 777 {}\n".format(logPath))
+	p.stdin.write("exit\n")
 	p.stdin.close()
+	p.wait()
 
 	cmd = "scp ubuntu@{ip}:{srcFile} ./{dstFile}".format(ip=ip, srcFile=logPath, dstFile=service)
+	print(cmd)
 	p = subprocess.Popen([cmd], shell=True).wait()
 
