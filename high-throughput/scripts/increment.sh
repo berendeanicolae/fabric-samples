db06ebaa52e6e1a76e19a7bdb52dbadbfa7f309a
berendeanicolae <berendeanicolae@gmail.com>
2019-04-24 21:17:20 +0300

    add experiment scripts

---
+++ high-throughput/scripts/increment.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+echo "========== Incrementing value #$3 peer$1_org$2 =========="
+export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org$2.example.com/users/Admin@org$2.example.com/msp
+export CORE_PEER_ADDRESS=peer$1.org$2.example.com:7051
+export CORE_PEER_LOCALMSPID="Org"$2"MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org$2.example.com/peers/peer$1.org$2.example.com/tls/ca.crt
+
+peer chaincode invoke -o orderer0.example.com:7050  --tls $CORE_PEER_TLS_ENABLED --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer0.example.com/msp/tlscacerts/tlsca.example.com-cert.pem  -C $CHANNEL_NAME -n $CC_NAME -c '{"Args":["increment","'$3'"]}'
---
+++ high-throughput/scripts/many-updates-thesis.py
@@ -0,0 +1,17 @@
+import subprocess
+import random
+
+peersCount = 10
+incCount = 10
+
+increments = []
+
+for peer in range(peersCount):
+    for inc in range(incCount):
+        increments.append((peer, inc))
+
+random.shuffle(increments)
+
+for increment in increments:
+    peer, value = increment
+    subprocess.Popen(["increment.sh {} {} {}".format(peer, 1, value)], shell=True).wait()
